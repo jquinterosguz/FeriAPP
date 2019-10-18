70dc508af0be738c7d020c19f7adc52217b9fd73
jquinterosguz <jquinterosguz@gmail.com>
2019-10-18 01:04:38 -0300

    esqueleto_v1

---
+++ src/components/FeriApp.vue
@@ -0,0 +1,21 @@
+<template>
+    <ion-grid>
+        <form>
+            <ion-col>
+            <ion-item>
+                <ion-label>Feria:</ion-label>
+                <ion-input name="feria"></ion-input>
+            </ion-item>
+            </ion-col>
+            <ion-col>
+                <ion-button type="submit" color="primary" expand="block">Encontrar</ion-button>
+            </ion-col>
+            </form>
+    </ion-grid>
+</template>
+
+<script>
+export default {
+    name:"EncontrarFeria"    
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import * as VueGoogleMaps from 'vue2-google-maps';
+import App from './App.vue';
+import router from './router';
+import Ionic from  '@ionic/vue';
+import '@ionic/core/css/ionic.bundle.css';
+
 
 Vue.config.productionTip = false
-
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyDLluDkfqWS0nAKOuHTH2miZHxsvE0ESxI",
+    libraries: "places" // necessary for places input
+  }
+});
+Vue.use(Ionic);
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
