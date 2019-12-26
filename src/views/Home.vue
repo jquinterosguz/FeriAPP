d793010591e785e502d0726e7e28a9cfca5db6f5
jquinterosguz <jquinterosguz@gmail.com>
2019-12-26 00:05:25 -0300

    Esqueleto_v2

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>FeriApp</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding"> 
+    <FeriApp/> 
+    <GoogleMaps/> 
+    </ion-content>
+  </div>
+</template>
+
+<script>
+import FeriApp from "../components/FeriApp";
+import GoogleMaps from "../components/GoogleMaps";
+export default {
+  name: 'home',
+  components: {FeriApp, GoogleMaps}
+}
+</script>
